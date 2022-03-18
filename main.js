21c4fe19eabbf5e76f49204e7cac82c570f8a062
Rusben <rusben@gmail.com>
2022-03-18 10:08:09 +0100

    Holakease

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var menu_icon = document.getElementById('menu-icon');
+
+console.log(menu_icon);
+
+menu_icon.addEventListener("click", toogleMenu);
+
+function toogleMenu() {
+  alert("Holakease");
+}
